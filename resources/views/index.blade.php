c277ef1026e616a5c9fe80eaae4bf8fa646467c4
Your Name <you@example.com>
2024-09-06 09:09:02 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CookPedia</title>
+</head>
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<body>
+    
+    <div class="container">
+
+        <!-- Navbar atas -->
+        <nav>
+            <div class="navbar">
+                <div class="logo">
+                    <p>CookPedia</p>
+                </div>
+                <div class="right-row">
+                    <div class="menuKanan">
+                        <ul>
+                            <li><a href="login.html">Login</a></li>
+                            <li><a href="register.html">Register</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Navbar Bawah -->
+            <div class="navbar">
+                    <div class="navbarBawah">
+                        <ul>
+                            <li><h1>TOP RATE</h1></li>
+                            <li><h1>MIE & PASTA</h1></li>
+                            <li><h1>QUICK</h1></li>
+                            <li><h1>LOW CRAB</h1></li>
+                            <li><h1>HIGH PROTEIN</h1></li>
+                        </ul>
+                    </div>
+            </div>
+        </nav>
+
+
+
+        <div class="opening">
+            <h1>Welcome to CookPedia</h1>
+        </div>
+    
+    </div>
+
+    <div class="container-resep">
+        <div class="resep">
+            
+            <div class="card-resep">
+                <div class="icon-resep">
+                <img src="{{ asset('img/padang.png') }}" alt="Logo">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="asset/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="asset/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="asset/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="asset/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="asset/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+            <div class="card-resep">
+                <div class="icon-resep">
+                    <img src="asset/padang.png" alt="">
+                </div>
+                <div class="desc-resep">
+                    <h2>Nasi padang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptas illum vitae. Dignissimos, ipsam obcaecati!</p><br>
+                    <a href="resep.html" class="card-button">Detail</a>
+                </div>
+            </div>
+
+        
+          
+            </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+</body>
+</html>
